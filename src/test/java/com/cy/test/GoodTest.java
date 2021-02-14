279c2c2ec97ba4baa8caa34c7238a246dd152ca4
tom_pro <goodMorning_pro@163.com>
2021-02-14 14:50:59 +0800

    提交到本地仓库

---
+++ src/test/java/com/cy/test/GoodTest.java
@@ -0,0 +1,20 @@
+/**
+* Copyright © 1998-2021, Glodon Inc. All Rights Reserved.
+*/
+package com.cy.test;
+
+/**
+ * 
+ * 此处填写类简介
+ * <p>
+ * 此处填写类说明
+ * </p>
+ * @author changy-c
+ * @since jdk1.6
+ * 2021年2月14日
+ *  
+ */
+
+public class GoodTest {
+
+}
